8f79f5a460bde9d8c4b3233bcbe084ca3bc3b028
李金木 <122771159@qq.com>
2025-06-05 15:18:22 +0800

    更新20250605151818

--- unpackage/dist/dev/mp-weixin/common/assets.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const _imports_0$2 = "/static/logo.png";
-const _imports_0$1 = "/static/images/noLogin.png";
-const _imports_0 = "/static/images/cart.gif";
-exports._imports_0 = _imports_0$2;
-exports._imports_0$1 = _imports_0$1;
-exports._imports_0$2 = _imports_0;
-//# sourceMappingURL=../../.sourcemap/mp-weixin/common/assets.js.map
